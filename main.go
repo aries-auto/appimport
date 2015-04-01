dd6b54cff33d418e92ab79b5736b69096172fcfc
Alex Ninneman <alex@ninneman.org>
2015-04-01 15:00:32 -0500

    added some stuff

--- main.go
+++ main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
-	"github.com/aries-auto/appimport/app/importer"
+	"github.com/aries-auto/appimport/importer"
 	"log"
+)
+
+var (
+	DataFiles = map[string]string{
+		"4 in bull bars, big horn":                  "csvs/four_inch_big_horn_bull_bars.csv",
+		"4 in round side bars, big step":            "csvs/four_inch_big_step_side_bars.csv",
+		"4 in oval side bars":                       "csvs/four_inch_oval_side_bars.csv",
+		"grille guards":                             "csvs/grille_guards.csv",
+		"pro series grille guards":                  "csvs/pro_series_grille_guards.csv",
+		"3 in bull bars":                            "csvs/three_inch_bull_bars.csv",
+		"3 in bull bars, pro series":                "csvs/three_inch_pro_series_bull_bars.csv",
+		"3 in round side bars, pro series":          "csvs/three_inch_pro_series_side_bars.csv",
+		"3 in round side bars":                      "csvs/three_inch_side_bars.csv",
+		"6 in oval side bars and mounting brackets": "csvs/six_inch_oval_side_bars.csv",
+	}
 )
 
 func main() {
 
-	// err := importer.DoImport("csvs/grille_guards.csv", "grille guards")
-	// if err != nil {
-	// 	log.Printf("Errored on Grille Guard: %s\n", err.Error())
-	// } else {
-	// 	log.Println("Finished Grille Guard")
-	// }
-
-	err := importer.DoImport("csvs/bull_bar.csv", "bull bar")
-	if err != nil {
-		log.Printf("Errored on Bull Bar: %s\n", err.Error())
-	} else {
-		log.Println("Finished Bull Bar")
+	for name, file := range DataFiles {
+		err := importer.DoImport(file, name)
+		if err != nil {
+			log.Printf("Errored on %s: %s\n", name, err.Error())
+		} else {
+			log.Printf("Finished %s", name)
+		}
 	}
 
 }
